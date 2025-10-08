55de9c31e2a0f725d917d89ffc01c743b42dc31f
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-10-08 16:42:32 +0700

    feat: shortcut keyboard events

--- src/components/layout/sidebar/app-sidebar-header/index.tsx
+++ src/components/layout/sidebar/app-sidebar-header/index.tsx
@@ -1,15 +1,44 @@
 "use client";
 import AppLogoComponent from "@/components/reusable/app-logo-component";
+import { Button } from "@/components/ui/button";
 import {
   SidebarHeader,
   SidebarTrigger,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { ROUTE_PATH } from "@/lib/enum/route-path";
 import { cn } from "@/lib/utils";
-import { PropsWithChildren } from "react";
+import { Search } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren, useEffect } from "react";
 
 const AppSidebarHeaderComponent = ({ children }: PropsWithChildren) => {
   const { open } = useSidebar();
+  const router = useRouter();
+  useEffect(() => {
+    const isMac = /Mac|iPhone|iPod|iPad/i.test(navigator.platform);
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const cmdOrCtrl = isMac ? event.metaKey : event.ctrlKey;
+      // Listen for Ctrl + K
+      if (cmdOrCtrl && event.key.toLowerCase() === "k") {
+        event.preventDefault(); // Prevent default browser behavior
+        router.push(`${ROUTE_PATH.HISTORY}?search=`);
+      }
+      // Listen for Ctrl + J
+      if (cmdOrCtrl && event.key.toLowerCase() === "j") {
+        event.preventDefault(); // Prevent default browser behavior
+        router.push(`${ROUTE_PATH.HOME}`);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
       <SidebarHeader
@@ -21,7 +50,15 @@ const AppSidebarHeaderComponent = ({ children }: PropsWithChildren) => {
         {open && <AppLogoComponent width={30} height={30} />}
         <SidebarTrigger>{!open && <AppLogoComponent />}</SidebarTrigger>
       </SidebarHeader>
-      {open ? children : null}
+      {open ? (
+        children
+      ) : (
+        <Link href={`${ROUTE_PATH.HISTORY}?search=`} className="px-2">
+          <Button variant={"outline"} className="w-full">
+            <Search />
+          </Button>
+        </Link>
+      )}
     </>
   );
 };
