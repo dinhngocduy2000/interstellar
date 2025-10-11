b547a08669532d6210728cd930cdc0dd2dd26e83
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-10-11 23:25:16 +0700

    feat: button to scroll to top of the list

--- src/app/(private)/history/(list-history-conversations)/index.tsx
+++ src/app/(private)/history/(list-history-conversations)/index.tsx
@@ -2,15 +2,19 @@
 
 import { useConversationListInfiniteQuery } from "@/lib/queries/conversation-query";
 import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
+import { useMemo, useRef, useState } from "react";
 import HistoryConversationItem from "./(history-conversation-item)";
 import { cn } from "@/lib/utils";
 import { Spinner } from "@/components/ui/spinner";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Virtuoso } from "react-virtuoso";
+import { Virtuoso, VirtuosoHandle } from "react-virtuoso";
+import { Button } from "@/components/ui/button";
+import { ArrowUp } from "lucide-react";
 const ListConversationHistory = () => {
   const params = useSearchParams();
   const searchText = params.get("search") ?? "";
+  const [isAtTop, setIsAtTop] = useState<boolean>(true);
+  const virtuosoRef = useRef<VirtuosoHandle>(null);
   const {
     data: listConversations,
     isFetchingNextPage,
@@ -30,6 +34,13 @@ const ListConversationHistory = () => {
     return listConversations?.pages.flatMap((page) => page.data) ?? [];
   }, [listConversations]);
 
+  const handleScrollToTop = () => {
+    virtuosoRef.current?.scrollToIndex({
+      index: 0,
+      behavior: "smooth",
+    });
+  };
+
   if (isFetching && !isFetchedAfterMount) {
     return (
       <div className="flex items-center h-full flex-col gap-4">
@@ -40,10 +51,12 @@ const ListConversationHistory = () => {
     );
   }
   return (
-    <div className="flex flex-col w-full h-full max-h-full overflow-auto">
+    <div className="flex flex-col w-full h-full max-h-full relative">
       <Virtuoso
+        ref={virtuosoRef}
         data={flatConversations}
         className="!h-full !w-full !pr-2"
+        atTopStateChange={setIsAtTop}
         fixedItemHeight={40}
         endReached={() => {
           fetchNextPage();
@@ -71,6 +84,15 @@ const ListConversationHistory = () => {
           />
         )}
       />
+      {!isAtTop && (
+        <Button
+          className="absolute bottom-0 -right-32 rounded-full size-12"
+          variant={"default"}
+          onClick={handleScrollToTop}
+        >
+          <ArrowUp />
+        </Button>
+      )}
     </div>
   );
 };
