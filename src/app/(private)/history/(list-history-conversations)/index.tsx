a2787d1e8f77b1884e8f35e53b058c2e5dbd63d7
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-10-11 22:52:13 +0700

    feat: adding virtual list to history page

--- src/app/(private)/history/(list-history-conversations)/index.tsx
+++ src/app/(private)/history/(list-history-conversations)/index.tsx
@@ -2,13 +2,12 @@
 
 import { useConversationListInfiniteQuery } from "@/lib/queries/conversation-query";
 import { useSearchParams } from "next/navigation";
-import { Fragment } from "react";
+import { useMemo } from "react";
 import HistoryConversationItem from "./(history-conversation-item)";
-import { useInfiniteScroll } from "@/lib/hooks/use-infinite-scroll";
 import { cn } from "@/lib/utils";
 import { Spinner } from "@/components/ui/spinner";
 import { Skeleton } from "@/components/ui/skeleton";
-
+import { Virtuoso } from "react-virtuoso";
 const ListConversationHistory = () => {
   const params = useSearchParams();
   const searchText = params.get("search") ?? "";
@@ -16,7 +15,6 @@ const ListConversationHistory = () => {
     data: listConversations,
     isFetchingNextPage,
     fetchNextPage,
-    hasNextPage,
     isLoading,
     isRefetching,
   } = useConversationListInfiniteQuery({
@@ -27,13 +25,11 @@ const ListConversationHistory = () => {
       title: searchText,
     },
   });
-  const { loaderRef: bottomScrollRef } = useInfiniteScroll({
-    onPageChange: () => {
-      fetchNextPage();
-    },
-    hasMore: hasNextPage,
-    isFetchingData: isFetchingNextPage,
-  });
+
+  const flatConversations = useMemo(() => {
+    return listConversations?.pages.flatMap((page) => page.data) ?? [];
+  }, [listConversations]);
+
   if (isLoading || isRefetching) {
     return (
       <div className="flex items-center h-full flex-col gap-4">
@@ -44,30 +40,40 @@ const ListConversationHistory = () => {
     );
   }
   return (
-    <div className="flex flex-col w-full max-h-full overflow-auto pr-2">
-      {listConversations?.pages.map((page, index) => (
-        <Fragment key={index}>
-          {page.data.map((conversation) => (
-            <HistoryConversationItem
-              key={conversation.id}
-              conversation={conversation}
-            />
-          ))}
-        </Fragment>
-      ))}
-      <div
-        ref={bottomScrollRef}
-        className={cn(
-          "h-2 flex max-w-fit max-h-fit items-center gap-2 mx-auto py-2",
+    <div className="flex flex-col w-full h-full max-h-full overflow-auto pr-2">
+      <Virtuoso
+        data={flatConversations}
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+        fixedItemHeight={40}
+        endReached={() => {
+          fetchNextPage();
+        }}
+        components={{
+          Footer: () => (
+            <div
+              className={cn(
+                "min-h-2 flex max-w-fit max-h-fit items-center gap-2 mx-auto py-2",
+              )}
+            >
+              {isFetchingNextPage && (
+                <>
+                  <Spinner />
+                  <p className="text-sm">Loading older conversations...</p>
+                </>
+              )}
+            </div>
+          ),
+        }}
+        itemContent={(_, conversation) => (
+          <HistoryConversationItem
+            key={conversation.id}
+            conversation={conversation}
+          />
         )}
-      >
-        {isFetchingNextPage && (
-          <>
-            <Spinner />
-            <p className="text-sm">Loading older conversations...</p>
-          </>
-        )}
-      </div>
+      />
     </div>
   );
 };
